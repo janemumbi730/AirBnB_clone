318ea351974ebbc0413d85906189d02d62810ca1
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-10 13:03:54 +0300

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -9,18 +9,19 @@ class TestAmenity(unittest.TestCase):
     def setUp(self):
         self.amenity = Amenity()
       
+    
+    def test_attributes_initialization(self):
+        self.assertEqual(self.amenity.name, "")
+        self.assertTrue(hasattr(self.amenity, "id"))
+        self.assertTrue(hasattr(self.amenity, "created_at"))
+        self.assertTrue(hasattr(self.amenity, "updated_at"))
+    
     def test_save_method(self):
         old_updated_at = self.amenity.updated_at
         self.amenity.save()
         new_updated_at = self.amenity.updated_at
         self.assertNotEqual(old_updated_at, new_updated_at)
   
-    def test_attributes_initialization(self):
-        self.assertEqual(self.amenity.name, "")
-        self.assertTrue(hasattr(self.amenity, "id"))
-        self.assertTrue(hasattr(self.amenity, "created_at"))
-        self.assertTrue(hasattr(self.amenity, "updated_at"))
-
     def test_str_method(self):
         amenity_str = str(self.amenity)
         self.assertIn("[Amenity]", amenity_str)
