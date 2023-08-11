24257c868edd36cafe3f74a3b4920fa4372cb559
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-11 13:41:31 +0300

    Create test_user.py

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Test module"""
+import unittest
+from models.user import User
+
+
+class TestUserClass(unittest.TestCase):
+    """Class User"""
+    def setUp(self):
+        self.user = User()
+
+    def tearDown(self):
+        del self.user
+
+    def test_to_dict_method(self):
+        """method of User"""
+        user_dict = self.user.to_dict()
+        self.assertIsInstance(user_dict, dict)
+        self.assertEqual(user_dict['__class__'], 'User')
+        self.assertTrue('id' in user_dict)
+        self.assertTrue('created_at' in user_dict)
+        self.assertTrue('updated_at' in user_dict)
+
+
+    def test_str_method(self):
+        """method of User"""
+        user_str = str(self.user)
+        expec_str = "[User] ({}) {}".format(self.user.id, self.user.__dict__)
+        self.assertEqual(user_str, expec_str)
+
+    def test_attributes(self):
+        """ttributes of User"""
+        self.assertTrue(hasattr(self.user, "email"))
+        self.assertTrue(hasattr(self.user, "password"))
+        self.assertTrue(hasattr(self.user, "first_name"))
+        self.assertTrue(hasattr(self.user, "last_name"))
+
+    def test_attributes_default_values(self):
+        """values of attributes"""
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
+
+    def test_instance_creation(self):
+        """correctly created user"""
+        self.assertIsInstance(self.user, User)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
