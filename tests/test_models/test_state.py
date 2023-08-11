cbde0a7b13bff2b0bf63e21dbb9ff4a9db76f852
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-11 13:36:10 +0300

    Create test_state.py

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,35 @@
+import unittest
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    def setUp(self):
+        self.state = State()
+
+    def tearDown(self):
+        self.state = None
+
+    def test_str_method(self):
+        state_str = str(self.state)
+        self.assertIn("[State]", state_str)
+        self.assertIn("id", state_str)
+        self.assertIn("created_at", state_str)
+        self.assertIn("updated_at", state_str)
+
+    def test_save_method(self):
+        old_updated_at = self.state.updated_at
+        self.state.save()
+        new_updated_at = self.state.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+
+    def test_attributes_initialization(self):
+        self.assertEqual(self.state.name, "")
+        self.assertTrue(hasattr(self.state, "id"))
+        self.assertTrue(hasattr(self.state, "created_at"))
+        self.assertTrue(hasattr(self.state, "updated_at"))
+
+
+
+if __name__ == "__main__":
+    unittest.main()
+  
