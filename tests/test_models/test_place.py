784eb2242bc9a0e7a1f1e928e922355c5a065524
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-12 21:41:32 +0300

    Update test_place.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,49 +1,48 @@
-import unittest
+#!/usr/bin/python3
+"""
+    module contains: Tests for Place class
+"""
 from models.place import Place
+import unittest
 
 
 class TestPlace(unittest.TestCase):
+    """ tests class Place """
     def setUp(self):
+        """ creates class instances """
         self.place = Place()
 
     def tearDown(self):
-        self.place = None
+        """ deletes instance """
+        del self.place
 
-    def test_attributes_initialization(self):
-        self.assertEqual(self.place.city_id, "")
-        self.assertEqual(self.place.user_id, "")
-        self.assertEqual(self.place.name, "")
-        self.assertEqual(self.place.description, "")
-        self.assertEqual(self.place.number_rooms, 0)
-        self.assertEqual(self.place.number_bathrooms, 0)
-        self.assertEqual(self.place.max_guest, 0)
-        self.assertEqual(self.place.price_by_night, 0)
-        self.assertEqual(self.place.latitude, 0.0)
-        self.assertEqual(self.place.longitude, 0.0)
-        self.assertEqual(self.place.amenity_ids, [])
-        self.assertTrue(hasattr(self.place, "id"))
-        self.assertTrue(hasattr(self.place, "created_at"))
-        self.assertTrue(hasattr(self.place, "updated_at"))
+    def test_attributes(self):
+        """ checks type and presence of attributes """
+        self.assertTrue(hasattr(self.place, 'name'))
+        self.assertTrue(hasattr(self.place, 'city_id'))
+        self.assertTrue(hasattr(self.place, 'user_id'))
+        self.assertTrue(hasattr(self.place, 'description'))
+        self.assertTrue(hasattr(self.place, 'number_rooms'))
+        self.assertTrue(hasattr(self.place, 'number_bathrooms'))
+        self.assertTrue(hasattr(self.place, 'max_guest'))
+        self.assertTrue(hasattr(self.place, 'price_by_night'))
+        self.assertTrue(hasattr(self.place, 'latitude'))
+        self.assertTrue(hasattr(self.place, 'longitude'))
+        self.assertTrue(hasattr(self.place, 'amenity_ids'))
 
-    def test_str_method(self):
-        place_str = str(self.place)
-        self.assertIn("[Place]", place_str)
-        self.assertIn("id", place_str)
-        self.assertIn("created_at", place_str)
-        self.assertIn("updated_at", place_str)
-
-    # def test_to_dict_method(self):
-    #     place_dict = self.place.to_dict()
-    #     self.assertTrue(isinstance(place_dict, dict))
-    #     self.assertEqual(place_dict["__class__"], "Place")
-    #     self.assertEqual(self.place.__dict__, place_dict)
-
-    def test_save_method(self):
-        old_updated_at = self.place.updated_at
-        self.place.save()
-        new_updated_at = self.place.updated_at
-        self.assertNotEqual(old_updated_at, new_updated_at)
-
+    def test_type_of_attributes(self):
+        """ checks that attributes are the right type """
+        self.assertIsInstance(self.place.name, str)
+        self.assertIsInstance(self.place.city_id, str)
+        self.assertIsInstance(self.place.user_id, str)
+        self.assertIsInstance(self.place.description, str)
+        self.assertIsInstance(self.place.number_rooms, int)
+        self.assertIsInstance(self.place.number_bathrooms, int)
+        self.assertIsInstance(self.place.max_guest, int)
+        self.assertIsInstance(self.place.price_by_night, int)
+        self.assertIsInstance(self.place.latitude, float)
+        self.assertIsInstance(self.place.longitude, float)
+        self.assertIsInstance(self.place.amenity_ids, list)
 
 if __name__ == "__main__":
     unittest.main()
