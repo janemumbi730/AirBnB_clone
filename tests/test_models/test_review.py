6d2ddc921aeb49a8c601813f33c1355b65174c4f
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-12 21:42:07 +0300

    Update test_review.py

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,41 +1,32 @@
-import unittest
+#!/usr/bin/python3
+"""
+    module contains: Tests for Review class
+"""
 from models.review import Review
+import unittest
 
 
 class TestReview(unittest.TestCase):
+    """ tests class Review """
     def setUp(self):
+        """ creates class instances """
         self.review = Review()
 
     def tearDown(self):
-        self.review = None
+        """ deletes instance """
+        del self.review
 
-    def test_attributes_initialization(self):
-        self.assertEqual(self.review.place_id, "")
-        self.assertEqual(self.review.user_id, "")
-        self.assertEqual(self.review.text, "")
-        self.assertTrue(hasattr(self.review, "id"))
-        self.assertTrue(hasattr(self.review, "created_at"))
-        self.assertTrue(hasattr(self.review, "updated_at"))
+    def test_attributes(self):
+        """ checks type and presence of attributes """
+        self.assertTrue(hasattr(self.review, 'text'))
+        self.assertTrue(hasattr(self.review, 'place_id'))
+        self.assertTrue(hasattr(self.review, 'user_id'))
 
-    def test_str_method(self):
-        review_str = str(self.review)
-        self.assertIn("[Review]", review_str)
-        self.assertIn("id", review_str)
-        self.assertIn("created_at", review_str)
-        self.assertIn("updated_at", review_str)
-
-    # def test_to_dict_method(self):
-    #     review_dict = self.review.to_dict()
-    #     self.assertTrue(isinstance(review_dict, dict))
-    #     self.assertEqual(review_dict["__class__"], "Review")
-    #     self.assertEqual(self.review.__dict__, review_dict)
-
-    def test_save_method(self):
-        old_updated_at = self.review.updated_at
-        self.review.save()
-        new_updated_at = self.review.updated_at
-        self.assertNotEqual(old_updated_at, new_updated_at)
-
+    def test_type_of_attributes(self):
+        """ checks that attributes are the right type """
+        self.assertIsInstance(self.review.text, str)
+        self.assertIsInstance(self.review.place_id, str)
+        self.assertIsInstance(self.review.user_id, str)
 
 if __name__ == "__main__":
     unittest.main()
