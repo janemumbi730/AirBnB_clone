8192454c6e5935ede998ff3f6d1f0039d2bc9336
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-12 20:57:48 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 
-"""class base model"""
-import models
-from datetime import datetime
+"""Create class BaseModel"""
+
 from uuid import uuid4
+from datetime import datetime
+import models
+
 
 class BaseModel:
     """
-    class base odel
+    class BaseModel
     """
     def __init__(self, *args, **kwargs):
-        """method"""
+        """Constructor method"""
         from models import storage
         if len(kwargs) == 0:
             self.id = str(uuid4())
@@ -27,22 +29,22 @@ class BaseModel:
                 else:
                     setattr(self, key, value)
 
-     def to_dict(self):
-        """values of instance returned"""
+    def __str__(self):
+        """String representation"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """Updates attr updated_at with current time"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """returns dictionary of key values of instance"""
         my_dict = self.__dict__.copy()
         # my_dict['id'] = self.id
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
         my_dict['__class__'] = self.__class__.__name__
         return my_dict
-       
-    def save(self):
-        """updates current time"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.save()
-
-    def __str__(self):
-        """represents str"""
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
