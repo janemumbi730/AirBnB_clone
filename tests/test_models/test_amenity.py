434c14f7ce1ee9d4bbcab69f6a237c9a300ed5f7
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-12 21:38:55 +0300

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,39 +1,28 @@
-import unittest
+#!/usr/bin/python3
+"""
+    module contains: Tests for Amenity class
+"""
 from models.amenity import Amenity
+import unittest
 
 
 class TestAmenity(unittest.TestCase):
+    """ tests class Amenity """
     def setUp(self):
+        """ creates class instances """
         self.amenity = Amenity()
 
     def tearDown(self):
-        self.amenity = None
+        """ deletes instance """
+        del self.amenity
 
-    def test_attributes_initialization(self):
-        self.assertEqual(self.amenity.name, "")
-        self.assertTrue(hasattr(self.amenity, "id"))
-        self.assertTrue(hasattr(self.amenity, "created_at"))
-        self.assertTrue(hasattr(self.amenity, "updated_at"))
+    def test_attributes(self):
+        """ checks type and presence of attributes """
+        self.assertTrue(hasattr(self.amenity, 'name'))
 
-    def test_str_method(self):
-        amenity_str = str(self.amenity)
-        self.assertIn("[Amenity]", amenity_str)
-        self.assertIn("id", amenity_str)
-        self.assertIn("created_at", amenity_str)
-        self.assertIn("updated_at", amenity_str)
-
-    # def test_to_dict_method(self):
-    #     amenity_dict = self.amenity.to_dict()
-    #     self.assertTrue(isinstance(amenity_dict, dict))
-    #     self.assertEqual(amenity_dict["__class__"], "Amenity")
-    #     self.assertEqual(self.amenity.__dict__, amenity_dict)
-
-    def test_save_method(self):
-        old_updated_at = self.amenity.updated_at
-        self.amenity.save()
-        new_updated_at = self.amenity.updated_at
-        self.assertNotEqual(old_updated_at, new_updated_at)
-
+    def test_type_of_attributes(self):
+        """ checks that attributes are the right type"""
+        self.assertIsInstance(self.amenity.name, str)
 
 if __name__ == "__main__":
     unittest.main()
