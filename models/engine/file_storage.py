174189eee518e4f15a7b8f982950415016c72ed4
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-12 21:24:50 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,62 +1,66 @@
 #!/usr/bin/python3
-
 """
-Create class FileStorage
+    module: FileStorage
 """
+import json
 from models.base_model import BaseModel
 from models.user import User
-from models.amenity import Amenity
+from models.state import State
 from models.city import City
+from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models.state import State
-import json
 
 
 class FileStorage:
     """
-    Class FileStorage
+        class FileStorage
+        private class attributes:
+            __file_path(str): path to the JSON file
+            __objects(dict): will store all objects by <class name>.id
     """
-    __file_path = "file.json"
+    __file_path = 'file.json'
     __objects = {}
+    classes = {"BaseModel": BaseModel,
+               "State": State,
+               "City": City,
+               "Amenity": Amenity,
+               "Place": Place,
+               "Review": Review,
+               "User": User}
 
     def all(self):
         """
-        returns the dictionary __objects
+            returns __objects
         """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """
-        sets in __objects the obj
+            sets in __objects the obj with key <obj class name>.id
         """
-        class_name = obj.__class__.__name__
-        self.__objects[f"{class_name}.{obj.id}"] = obj
+        key = obj.__class__.__name__ + '.' + str(obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """
-        serializes __objects to the JSON file
+            serializes __objects to the JSON file
         """
-        non_ser_dict = self.__objects
-        ser_dict = {}
-
-        for obj_id in non_ser_dict.keys():
-            ser_dict[obj_id] = non_ser_dict[obj_id].to_dict()
-
-        with open(self.__file_path, "w") as json_file:
-            json.dump(ser_dict, json_file)
+        my_dict = {}
+        for k, v in self.__objects.items():
+            my_dict[k] = v.to_dict()
+        with open(FileStorage.__file_path, mode='w', encoding='UTF-8') as f:
+            json.dump(my_dict, f)
 
     def reload(self):
         """
-        deserializes the JSON file to __objects
+            deserializes the JSON file to __objects
         """
         try:
-            with open(self.__file_path) as json_file:
-                ser_dict = json.load(json_file)
-                for values in ser_dict.values():
-                    cls_name = values["__class__"]
-                    del values["__class__"]
-                    self.new(eval(cls_name)(**values))
-
+            with open(FileStorage.__file_path, mode="r") as f:
+                new_dict = json.load(f)
+                for k, v in new_dict.items():
+                    base = FileStorage.classes[v["__class__"]](**v)
+                    FileStorage.__objects[k] = base
         except FileNotFoundError:
-            return
+            pass
