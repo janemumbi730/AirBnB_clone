d92b7adee11592391e06f8a44b1a9a86b62555f8
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-12 21:43:09 +0300

    Update test_user.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,50 +1,34 @@
 #!/usr/bin/python3
-"""Tests for user.py module"""
-import unittest
+"""
+    module contains: Tests for User class
+"""
 from models.user import User
+import unittest
 
 
-class TestUserClass(unittest.TestCase):
-    """Unittests for Class User"""
+class TestUser(unittest.TestCase):
+    """ tests class User """
     def setUp(self):
+        """ creates class instances """
         self.user = User()
 
     def tearDown(self):
+        """ deletes instance """
         del self.user
 
-    def test_instance_creation(self):
-        """Test if an instance of User is correctly created"""
-        self.assertIsInstance(self.user, User)
-
     def test_attributes(self):
-        """Test the attributes of User"""
-        self.assertTrue(hasattr(self.user, "email"))
-        self.assertTrue(hasattr(self.user, "password"))
-        self.assertTrue(hasattr(self.user, "first_name"))
-        self.assertTrue(hasattr(self.user, "last_name"))
+        """ checks type and presence of attributes """
+        self.assertTrue(hasattr(self.user, 'email'))
+        self.assertTrue(hasattr(self.user, 'first_name'))
+        self.assertTrue(hasattr(self.user, 'last_name'))
+        self.assertTrue(hasattr(self.user, 'password'))
 
-    def test_attributes_default_values(self):
-        """Test the default values of attributes"""
-        self.assertEqual(self.user.email, "")
-        self.assertEqual(self.user.password, "")
-        self.assertEqual(self.user.first_name, "")
-        self.assertEqual(self.user.last_name, "")
+    def test_type_of_attributes(self):
+        """ checks that attributes are the right type """
+        self.assertIsInstance(self.user.email, str)
+        self.assertIsInstance(self.user.first_name, str)
+        self.assertIsInstance(self.user.last_name, str)
+        self.assertIsInstance(self.user.password, str)
 
-    def test_to_dict_method(self):
-        """Test the to_dict method of User"""
-        user_dict = self.user.to_dict()
-        self.assertIsInstance(user_dict, dict)
-        self.assertEqual(user_dict['__class__'], 'User')
-        self.assertTrue('id' in user_dict)
-        self.assertTrue('created_at' in user_dict)
-        self.assertTrue('updated_at' in user_dict)
-
-    def test_str_method(self):
-        """Test the __str__ method of User"""
-        user_str = str(self.user)
-        expec_str = "[User] ({}) {}".format(self.user.id, self.user.__dict__)
-        self.assertEqual(user_str, expec_str)
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
