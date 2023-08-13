dfcbcc48be677094892ce27588c0d08150ceadea
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-13 14:12:31 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 """
-    module: FileStorage
+   Create class FileStorage
 """
-import json
 from models.base_model import BaseModel
 from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.city import City
 from models.place import Place
 from models.review import Review
+from models.state import State
+import json
+
 
 
 class FileStorage:
     """
         class FileStorage
-        private class attributes:
-            __file_path(str): path to the JSON file
-            __objects(dict): will store all objects by <class name>.id
+        attributes:
+            __file_path: path to JSON file
+            __objects: will store all objects
     """
     __file_path = 'file.json'
     __objects = {}
@@ -29,18 +30,21 @@ class FileStorage:
                "Review": Review,
                "User": User}
 
+
     def all(self):
         """
-            returns __objects
+            returns the dictionary __objects
         """
         return FileStorage.__objects
 
+
     def new(self, obj):
         """
-            sets in __objects the obj with key <obj class name>.id
+            sets in __objects the obj
         """
         key = obj.__class__.__name__ + '.' + str(obj.id)
         FileStorage.__objects[key] = obj
+
 
     def save(self):
         """
@@ -51,6 +55,7 @@ class FileStorage:
             my_dict[k] = v.to_dict()
         with open(FileStorage.__file_path, mode='w', encoding='UTF-8') as f:
             json.dump(my_dict, f)
+
 
     def reload(self):
         """
