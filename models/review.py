9e9a77aeb7ec66ae279960a4efd3e2335a13d001
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2023-08-13 22:26:47 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,15 +1,15 @@
-#!usr/bin/python3
-"""Module: Review"""
+#!/usr/bin/python3
+
+"""
+Create class Review
+"""
+
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
     """
         class Review
-        public attributes:
-            name(str): name of review
-            user(int): user_id
-            name(str): text
     """
 
     place_id = ""
